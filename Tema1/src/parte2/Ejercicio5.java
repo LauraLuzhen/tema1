de0aadd3ff8729698666a371e8212da46adeb93a
laura <laura@10.100.213.223>
2024-09-30 18:25:48 +0200

    Tema 1 Actividades

--- Tema1/src/parte2/Ejercicio5.java
+++ Tema1/src/parte2/Ejercicio5.java
@@ -10,9 +10,26 @@ public class Ejercicio5 {
 		Scanner reader = new Scanner (System.in);
 		
 		//Declaración de variables
+		int segundosUsuario;
+		int horas;
+		int minutos;
 		int segundos;
 		
 		//Le pedimos al usuario
+		System.out.println("Dime los segundos: ");
 		
+		//La consola espera por una respuesta
+		segundosUsuario = reader.nextInt();
+		
+		//Calculamos las horas, minutos y segundos
+		horas = segundosUsuario / 3600;
+		minutos = (segundosUsuario % 3600) / 60;
+		segundos = (segundosUsuario % 3600) % 60;
+		
+		//Imprimimos por pantalla
+		System.out.println(segundos + " segundos son: " + horas + " horas, " + minutos + " minutos y " + segundos + " segundos.");
+		
+		//Cerramos la consola
+		reader.close();
 	}
 }
